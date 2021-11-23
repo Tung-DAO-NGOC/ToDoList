3c85c9cd0552873a2887d8d50a5f352ee59c50e9
Tungdn30 <tungdn30.weo@gmail.com>
2021-11-23 20:43:34 +0700

    Edit title Todo

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,18 @@
 // Truy cập thành phần
 const todo_listEl = document.querySelector(".todo-list");
 const todo_inputEl = document.querySelector("#todo-input");
+const todo_editInputEl = document.querySelector("#edit-input");
+const todo_editIndexEl = document.querySelector("#edit-id");
 const todo_btnAddEl = document.querySelector("#btn-add");
+const todo_btnEditEl = document.querySelector("#btn-edit");
+const todo_addDivEl = document.querySelector(".todo-div");
+const todo_editDivEl = document.querySelector(".edit-div");
+const radio_all = document.querySelector("#all");
+const radio_unactive = document.querySelector("#unactive");
+const radio_active = document.querySelector("#active");
 // Khai báo biến
 let todos = [];
+let disable_radio = false;
 
 function createID() {
 	return Math.floor(Math.random() * 100000);
@@ -48,9 +57,16 @@ function deleteTodosAPI(id) {
 // Patch Todo - Status
 
 function patchStatusTodosAPI(id, statusInput) {
-	console.log(statusInput);
 	return axios.patch(`/todos/${id}`, {
 		status: statusInput,
+	});
+}
+
+// Patch Todo - Title
+
+function patchTitleTodosAPI(id, titleInput) {
+	return axios.patch(`/todos/${id}`, {
+		title: titleInput,
 	});
 }
 
@@ -79,7 +95,7 @@ async function postTodos(title) {
 }
 
 todo_btnAddEl.addEventListener(`click`, function () {
-	let todoTitle = todo_inputEl.value;
+	const todoTitle = todo_inputEl.value;
 	if (todoTitle === "") {
 		alert("Tiêu đề không được để trống");
 		return;
@@ -96,14 +112,42 @@ async function deleteTodos(id) {
 	}
 }
 
-async function changeStatusTodos(id, status) {
+async function changeStatusTodos(id) {
 	try {
-		const res = await patchStatusTodosAPI(id, !status ? true : false);
+		const status = todos.find((todo) => todo.id === id).status;
+		const res = await patchStatusTodosAPI(id, !status);
 		getTodos();
 	} catch (error) {
 		console.log(error);
 	}
 }
+
+async function changeTitleTodos(id, title) {
+	try {
+		const res = await patchTitleTodosAPI(id, title);
+		getTodos();
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function editTodos(id) {
+	toggleRadio();
+	const editTodo = todos.find((todo) => todo.id === id);
+	todo_editDivEl.classList.remove("hidden");
+	todo_addDivEl.classList.add("hidden");
+	todo_editInputEl.value = editTodo.title;
+	todo_editIndexEl.value = editTodo.id;
+}
+
+todo_btnEditEl.addEventListener(`click`, function () {
+	const editTitle = todo_editInputEl.value;
+	const editID = todo_editIndexEl.value;
+	changeTitleTodos(editID, editTitle);
+	todo_editDivEl.classList.add("hidden");
+	todo_addDivEl.classList.remove("hidden");
+	toggleRadio();
+});
 
 // Render UI - hiển thị danh sách Todo ra ngoài giao diện
 
@@ -117,16 +161,16 @@ function renderUI(arr) {
 			todo_listEl.innerHTML += `
                 <div class="todo-item ${work.status ? `active-todo` : ``}">
                     <div class="todo-item-title">
-                        <input type="checkbox" ${work.status ? `checked` : ``} onclick="changeStatusTodos(${work.id}, ${
-				work.status
+                        <input type="checkbox" ${work.status ? `checked` : ``} onclick="changeStatusTodos(${
+				work.id
 			})" />
                         <p>${work.title}</p>
                      </div>
                      <div class="option">
-                         <button class="btn btn-update">
+                         <button class="btn btn-update" onclick="editTodos(${work.id})">
                             <img src="./img/pencil.svg" alt="icon" />
                          </button>
-                         <button class="btn btn-delete" onclick=deleteTodos(${work.id})>
+                         <button class="btn btn-delete" onclick="deleteTodos(${work.id})">
                             <img src="./img/remove.svg" alt="icon" />
                           </button>
                      </div>
@@ -134,6 +178,13 @@ function renderUI(arr) {
             `;
 		}
 	}
+}
+
+function toggleRadio() {
+	disable_radio = !disable_radio;
+	radio_active.disabled = disable_radio;
+	radio_unactive.disabled = disable_radio;
+	radio_all.disabled = disable_radio;
 }
 
 // Main
